2539216ed8f1fc3879b8db27ebaba982cdd55f44
pashaadel <Pasha.adel2@gmail.com>
2022-07-24 20:30:28 +0430

    befor completing the second page

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+from email.mime import image
+from msilib.schema import File
+from pydoc import cli
 from tkinter import *
 import random
 import webbrowser
 import datetime
 import re
 from tkinter import messagebox
+import captcha
+from captcha.image import ImageCaptcha
+from PIL import ImageTk, Image
+import click
+
 
 # app setting
 
 win = Tk()
-win.title("login")
+win.title("Sign up")
 win.geometry("500x500")
 win.resizable(False, False)
 app_icon = PhotoImage(file="app-icon.jpg")
 win.iconphoto(False, app_icon)
+
 
 # head frame
 
@@ -21,7 +30,7 @@ head_frame.pack()
 
 # email & password
 
-entry_lbl = Label(head_frame, text="Fill the blanks to login", font=("courier", 15))
+entry_lbl = Label(head_frame, text="Fill the blanks to sign up", font=("courier", 15))
 entry_lbl.pack(pady=20)
 
 email_input_lbl = Label(head_frame, text="Email", font=("courier, 10"))
@@ -54,30 +63,38 @@ def check(email):
 
 # captcha
 
-
 def captcha_func():
-    global captcha, captcha_text
+    global captcha_txt, captcha_text, data_1, captcha_image, my_image
     captcha_text = str(random.randint(10000, 99999))
-    captcha = ""
+    captcha_txt = ""
     for item in captcha_text:
-        captcha += "  "
-        captcha += item
-
+        captcha_txt += "  "
+        captcha_txt += item
+    
+    captcha_image = ImageCaptcha()
+    data_1 = captcha_image.generate(captcha_txt)
+    captcha_image.write(captcha_txt, 'captcha.png')
+    my_image = ImageTk.PhotoImage(Image.open("captcha.png"))
 
 captcha_func()
 
-
-def change_captcha():
-    captcha_func()
-    captcha_lbl.config(text=captcha)
-
-
-captcha_lbl = Button(head_frame, text=captcha, font=("courier, 12"), cursor="hand2", bd=0,
-                     command=change_captcha)
-captcha_lbl.pack(pady=15)
+captcha_lbl = Label(head_frame, image=my_image)
+captcha_lbl.pack(pady=10)
 
 captcha_entry = Entry(head_frame)
 captcha_entry.pack()
+
+# recaptcha
+
+def change_captcha():
+    captcha_func()
+    captcha_lbl.config(image=my_image)
+
+click_image = PhotoImage(file="test.png")
+
+recaptcha_btn = Button(head_frame, width=30,height=30, image=click_image, command=change_captcha)
+recaptcha_btn.place(x=240, y=215)
+
 
 # password validation
 password_regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{8,18}$"
@@ -87,7 +104,7 @@ def pass_validation():
     global captcha
     if re.search(match_re, password_input.get()):
         if password_input.get() == password_confirm_input.get():
-            if captcha_entry.get() == captcha:
+            if captcha_entry.get() == captcha_text:
                 messagebox.showinfo("Logged in", "You successfully logged in!")
             else:
                 messagebox.showerror("captcha error", "Please fill the captcha blank to submit that you are not a bot!!")
@@ -96,10 +113,10 @@ def pass_validation():
     else:
         messagebox.showerror("password error", "Invalid password!!")
 
-# login button func
+# sign_up button func
 
 
-def login():
+def sign_up():
     email = email_input.get()
     if check(email) == False:
         messagebox.showerror("email error", "wrong email!! try again")
@@ -107,14 +124,14 @@ def login():
         pass_validation()
 
 
-# login button
+# sign up button
 
-login_btn = Button(head_frame, text="login", width=20, bd=5, relief="ridge", command=login)
-login_btn.pack(pady=20)
+sign_up_btn = Button(head_frame, text="sign up", width=20, bd=5, relief="ridge", command=sign_up)
+sign_up_btn.pack(pady=20)
 
 # forget password
 
-forget_password = Label(head_frame, text="Forgot password?", fg="blue", cursor="hand2")
+forget_password = Label(head_frame, text="Already have an account?", fg="blue", cursor="hand2")
 forget_password.pack()
 
 
@@ -142,7 +159,7 @@ time_date = time.strftime("%Y/%m/%d")
 time_now = time.strftime("%H:%M:%S")
 
 time_lbl = Label(win, text=time_date, font=("courier", 15), borderwidth=2, relief="solid")
-time_lbl.pack(pady=30)
+time_lbl.pack(pady=20)
 
 date_lbl = Label(win, text=time_now, font=("courier", 15), borderwidth=2, relief="solid")
 date_lbl.pack(pady=0)
